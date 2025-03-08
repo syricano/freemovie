b9971e81613bb082f4b7838af79da3a8f0247011
Anas <syriacano@gmail.com>
2025-03-08 12:24:12 +0100

    fixed style issue and links to scripts and tailwind

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const apiUrl = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
 
-// 🔹 Replace with your actual TMDB Bearer Token
 const apiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzI4OTczODY3ZjIwZTdlZDlkMjIyZmUyNzhkMWU4OCIsIm5iZiI6MTc0MDgxODA0Mi43ODksInN1YiI6IjY3YzJjNjdhNmU3NjgwOTAwNzZkZGE1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EjxNAWUwkXEasfvKDxpugn3w6uWolkq2O3Ae8Y656go';
 
 const options = {
@@ -54,3 +53,7 @@ function displayMovies(movies) {
 
 // fetch popular movies when reload the page
 document.addEventListener('DOMContentLoaded', fetchPopularMovies);
+
+if (typeof document !== 'undefined') {
+  document.addEventListener('DOMContentLoaded', fetchPopularMovies);
+}
